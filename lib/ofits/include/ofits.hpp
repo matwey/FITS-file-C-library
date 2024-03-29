ce0420c4d18c146780d2037254a680b8ec3863ae
cilantrofe <131597495+cilantrofe@users.noreply.github.com>
2024-03-29 22:58:51 +0300

    Add write_data

--- lib/ofits/include/ofits.hpp
+++ lib/ofits/include/ofits.hpp
@@ -12,12 +12,30 @@ class ofits
 {
 public:
     ofits() = default;
-    ofits(boost::asio::io_context &io_context, const std::filesystem::path &filename, std::initializer_list<std::initializer_list<std::size_t>> schema)
+    ofits(boost::asio::io_context &io_context, const std::filesystem::path &filename, std::array<std::initializer_list<std::size_t>, sizeof...(Args)> schema)
         : file_(io_context, filename, boost::asio::random_access_file::write_only),
-          hdus_(std::apply([&](auto &&...args)
-                           { return std::make_tuple(hdu<Args>(*this, args)...); },
-                           schema))
+          offset_(0),
+          hdus_{std::apply([&](auto &&...args)
+                           { return std::make_tuple(hdu<Args>(*this, offset_, args)...); },
+                           schema)}
     {
+    }
+
+    void update_offset(std::size_t offset)
+    {
+        offset_ = offset;
+    }
+
+    template <std::size_t N, class T>
+    void value_as(std::string_view key, T value) const
+    {
+        std::get<N>(hdus_).template value_as<T>(key, value);
+    }
+
+    template <std::size_t N>
+    void write_data(const std::vector<typename std::tuple_element<N, std::tuple<Args...>>::type> &data)
+    {
+        std::get<N>(hdus_).write_data(data);
     }
 
     template <std::size_t N>
@@ -30,40 +48,115 @@ public:
     class hdu
     {
         hdu() = default;
-        hdu(ofits &parent_ofits, const std::initializer_list<std::size_t> &hdu_schema)
-            : parent_ofits_(parent_ofits), header_block_(2880, ' '), headers_written_(0)
+
+        static constexpr auto kSizeHeaderBlock = 2880;
+
+    public:
+        hdu(ofits &parent_ofits, std::size_t offset, const std::initializer_list<std::size_t> &hdu_schema)
+            : parent_ofits_(parent_ofits), headers_written_(0), header_block_(2880, ' '), offset_(offset), data_offset_(0)
         {
             write_header("SIMPLE", "T");
-            write_header("BITPIX", std::to_string(get_bitpix_for_type()));
+
+            int bitpix = get_bitpix_for_type();
+            write_header("BITPIX", std::to_string(bitpix));
+
             write_header("NAXIS", std::to_string(hdu_schema.size()));
+
+            std::size_t naxis_product = 1;
 
             std::size_t i = 0;
             for (const auto &size : hdu_schema)
             {
+                naxis_product *= size;
                 write_header("NAXIS" + std::to_string(++i), std::to_string(size));
             }
 
             write_header("EXTEND", "T");
+
             write_header("END", "");
+
+            data_block_size_ = (((naxis_product * std::abs(bitpix) / 8) + kSizeHeaderBlock - 1) / kSizeHeaderBlock) * kSizeHeaderBlock;
+
+            parent_ofits_.update_offset(round_offset(80 * headers_written_) + data_block_size_);
         }
 
+        template <class U>
+        void value_as(const std::string_view &key, const U &value) const
+        {
+            if (headers_written_ * 80 < header_block_.size())
+            {
+                std::string header = std::string(key) + " = " + std::string(value);
+                header.resize(80, ' ');
+
+                size_t position = headers_written_ * 80 + offset_; // перезаписываю END
+                boost::asio::write_at(parent_ofits_.file_, position, boost::asio::buffer(header));
+
+                ++headers_written_;
+
+                // пишу END
+                header = "END";
+                header.resize(80, ' ');
+
+                position = headers_written_ * 80 + offset_;
+                boost::asio::write_at(parent_ofits_.file_, position, boost::asio::buffer(header));
+            }
+            else
+            {
+                throw std::runtime_error("Not enough space in the HDU");
+            }
+        }
+
+        void write_data(const std::vector<T> &data)
+        {
+            size_t data_size = data.size() * sizeof(T);
+
+            if (data_size < data_block_size_)
+            {
+                boost::asio::write_at(parent_ofits_.file_, kSizeHeaderBlock + offset_ + data_offset_ /*data_offset_ храним, чтобы смогли еще записать данные*/ , boost::asio::buffer(data.data(), data_size));
+                data_offset_ += data_size;
+
+            } else {
+                 throw std::runtime_error("Not enough space in the HDU");
+            }
+        }
+
+        // для проверки
+        std::size_t get_headers_written() const
+        {
+            return headers_written_;
+        }
+
+    private:
         void write_header(const std::string &key, const std::string &value)
         {
+            if (key == "END")
+            {
+                std::string header = key;
+                header.resize(80, ' ');
+
+                size_t position = headers_written_ * 80 + offset_;
+                boost::asio::write_at(parent_ofits_.file_, position, boost::asio::buffer(header));
+
+                return;
+            }
+
             std::string header = key + " = " + value;
             header.resize(80, ' ');
-            size_t position = headers_written_ * 80;
-            boost::asio::write_at(parent_ofits_.file_, position, boost::asio::buffer(header));
-            ++headers_written_;
+
+            if (headers_written_ * 80 < header_block_.size())
+            {
+                size_t position = headers_written_ * 80 + offset_;
+                boost::asio::write_at(parent_ofits_.file_, position, boost::asio::buffer(header));
+
+                ++headers_written_;
+            }
+            else
+            {
+                throw std::runtime_error("Not enough space in the HDU");
+            }
         }
 
-        
-        // проверить на наличие места и записать
-        template <class T>
-        void value_as(string_view key, T value) const {
-            // запись хэдеров
-        }
-
-        static int get_bitpix_for_type()
+        int get_bitpix_for_type() const
         {
             if constexpr (std::is_same_v<T, std::uint8_t>)
             {
@@ -95,13 +188,22 @@ public:
             }
         }
 
+        static std::size_t round_offset(const std::size_t &offset)
+        {
+            return (offset % kSizeHeaderBlock == 0) ? offset : (offset / kSizeHeaderBlock + 1) * kSizeHeaderBlock;
+        }
+
     private:
         ofits &parent_ofits_;
         std::string header_block_;
-        size_t headers_written_;
+        mutable std::size_t headers_written_;
+        std::size_t offset_;
+        std::size_t data_block_size_;
+        std::size_t data_offset_;
     };
 
 private:
     boost::asio::random_access_file file_;
+    std::size_t offset_;
     std::tuple<hdu<Args>...> hdus_;
 };
--- lib/tests/test_ofits.cpp
+++ lib/tests/test_ofits.cpp
@@ -42,3 +42,22 @@ TEST(ofits_test, check_double_hdu)
 
     EXPECT_EQ(hdu_1.get_headers_written(), 8);
 }
+
+TEST(ofits_test, check_data)
+{
+    boost::asio::io_context io_context;
+
+    ofits<std::uint8_t, float> double_hdu_data_file{io_context, DATA_ROOT "/double_hdu_data.fits", {{{200, 300}, {100, 50, 50}}}};
+
+    std::vector<std::uint8_t> data_uint8_t = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+
+    double_hdu_data_file.write_data<0>(data_uint8_t);
+
+    std::vector<std::uint8_t> data2_uint8_t = {110, 120, 130, 140, 150, 160 };
+
+    double_hdu_data_file.write_data<0>(data2_uint8_t);
+
+    std::vector<float> data_float = {0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1.0f};
+
+    double_hdu_data_file.write_data<1>(data_float);
+}
